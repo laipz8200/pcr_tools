b1910bbeba683f8b6765d3157a9d55fe9d31a685
laipz8200 <laipz8200@outlook.com>
2020-05-31 15:47:15 +0800

    remove print info

--- app/main/routes.py
+++ app/main/routes.py
@@ -19,7 +19,6 @@ def index(img_name=None):
         equip = form.equip.data
 
         c_id = name2id(name)
-        print(name, star,rank, equip, c_id)
         if not os.path.exists(f'app/static/{star}X_R{rank}_{c_id}{"E" if equip else ""}.png'):
             try:
                 img = genicon(name, star, rank, equip)
